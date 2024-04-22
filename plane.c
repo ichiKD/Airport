90bf39142dfba22bfb889080b05bc5cdcd640b68
ichiKD <shubhangjain2001@gmail.com>
2024-04-23 01:30:43 +0530

    Plane2

--- plane.c
+++ plane.c
@@ -11,10 +11,21 @@
 #include <pthread.h>
 
 
+struct Plane{
+    int arrival_ariport, departure_airport;
+    int plane_id, total_weight;
+    int plane_type, passengers; 
+};
+
+
 int main(){
-    int plane_id, plane_type, passengers;
+    int plane_id, plane_type, passengers=0;
+    int total_weight=0;
+    int num_cargo=0, avg_cargo_weight=0;
     int luggage_weight[20]={0};
     int passenger_weight[20]={0};
+    int departure_airport, arrival_ariport;
+
     int fd[20][2]; //pipe
     printf("Enter Plane ID: ");
     fflush(stdout);
@@ -78,9 +89,13 @@ int main(){
         else{
             for(int i=0; i<passengers; i++){
                 read(fd[i][0], &luggage_weight[i], sizeof(int));
+                total_weight+=luggage_weight[i] + passenger_weight[i];
             }
             for(int i=0; i<passengers; i++){
                 read(fd[i][0], &passenger_weight[i], sizeof(int));
+            }
+            for(int i=0; i<passengers; i++){
+                total_weight += luggage_weight[i] + passenger_weight[i];
             }
             for(int i=0; i<passengers; i++){
                 printf("%d\n", luggage_weight[i]);
@@ -91,13 +106,47 @@ int main(){
             while (wait(NULL) != -1){
                 ;
             }
+            total_weight += (75 *7);
         }
     }
     else{
         // cargo plane
+        printf("Enter Number of Cargo Items: ");
+        fflush(stdout);
+        scanf("%d", &num_cargo);
+        printf("\n");
+        fflush(stdout);
+        printf("Enter Average Weight of Cargo Items: ");
+        fflush(stdout);
+        scanf("%d", &avg_cargo_weight);
+        printf("\n");
+        fflush(stdout);
+        total_weight = (avg_cargo_weight * num_cargo) + (75 * 2);
     }
 
+    printf("Enter Airport Number for Departure: ");
+    fflush(stdout);
+    scanf("%d", &departure_airport);
+    printf("\n");
+    fflush(stdout);
 
+    printf("Enter Airport Number for Arrival: ");
+    fflush(stdout);
+    scanf("%d", &arrival_ariport);
+    printf("\n");
+    fflush(stdout);
+
+
+    struct Plane plane_data;
+    plane_data.arrival_ariport   = arrival_ariport;
+    plane_data.departure_airport = departure_airport;
+    plane_data.plane_id          = plane_id;
+    plane_data.total_weight      = total_weight;
+    plane_data.plane_type        = plane_type;
+    plane_data.passengers        = passengers;
+
+
+    //send the plane_data to message queue
 
 
     return 0;
