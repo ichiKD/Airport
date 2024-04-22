180b764b2d792484b7e8f6423960170ddf06a897
ichiKD <shubhangjain2001@gmail.com>
2024-04-22 13:55:53 +0530

    First commit

---
+++  airport.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <fcntl.h> 
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+
+int main(){
+    return 0;
+}
