292f2ac1254ee82f5e3bb24661b003d675386949
ichiKD <shubhangjain2001@gmail.com>
2024-04-23 13:59:53 +0530

    Airport

---  airport.c
+++  airport.c
@@ -24,9 +24,10 @@ const int message_sender_id = 3;
 
 
 int airport_number, runways;
-int *loadCapacity = NULL;
-int *busy = NULL;
-int back_up_BUSY = 0;
+pthread_t *threads = NULL;
+int *loadCapacity  = NULL;
+int *busy          = NULL;
+int back_up_BUSY   = 0;
 
 
 struct Plane{
@@ -34,8 +35,65 @@ struct Plane{
     int plane_id, total_weight;
     int plane_type, passengers; 
 };
+struct ThreadArgs {
+    int idx;
+    int FOR_DEPARTURE;
+    struct Plane plane;
+};
 
 
+
+
+void * useRunways(void *args){
+    struct ThreadArgs *threadArg = (struct ThreadArgs *) args;
+    int idx                      = threadArg->idx;
+    int FOR_DEPARTURE            = threadArg->FOR_DEPARTURE;
+    struct Plane r               = threadArg->plane;
+
+    if(idx == runways){
+        //backup runway
+        back_up_BUSY = 1;
+        if(FOR_DEPARTURE == 1){
+            sleep(3);
+            printf("Plane %d has completed boarding/loading and taken off from Runway No. %d of Airport No. %d.\n",
+                    r.plane_id, idx + 1, airport_number);
+            // SEND CONFORMATION TO ATC about boarding
+            sleep(2);
+            // SEND CONFORMATION TO ATC about Takeoff
+        }
+        else if(FOR_DEPARTURE == 0){
+            sleep(2);
+            // SEND CONFORMATION TO ATC about landing
+            sleep(3);
+            printf("Plane %d has landed on Runway No. %d of Airport No. %d and has completed deboarding/unloading.\n",
+                    r.plane_id, idx + 1, airport_number);
+            // SEND CONFORMATION TO ATC about deboarded
+        }
+        back_up_BUSY = 0;
+        pthread_exit(NULL);
+        return;
+    }
+
+    busy[idx] = 1;
+    if(FOR_DEPARTURE == 1){
+        sleep(3);
+        printf("Plane %d has completed boarding/loading and taken off from Runway No. %d of Airport No. %d.\n",
+                r.plane_id, idx + 1, airport_number);
+        // SEND CONFORMATION TO ATC about boarding
+        sleep(2);
+        // SEND CONFORMATION TO ATC about Takeoff
+    }
+    else if(FOR_DEPARTURE == 0){
+        sleep(2);
+        // SEND CONFORMATION TO ATC about landing
+        sleep(3);
+        printf("Plane %d has landed on Runway No. %d of Airport No. %d and has completed deboarding/unloading.\n",
+                r.plane_id, idx + 1, airport_number);
+        // SEND CONFORMATION TO ATC about deboarded
+    }
+    busy[idx] = 0;
+    pthread_exit(NULL);
+}
 
 
 int main(){
@@ -51,9 +109,9 @@ int main(){
     printf("\n");
     printf("Enter loadCapacity of Runways (give as a space separated list in a single line):\n");
     fflush(stdout);
-
-    loadCapacity = (int*) malloc (runways * sizeof(int));
-    busy         = (int*) malloc (runways * sizeof(int));
+    threads      = (pthread_t *) malloc ((runways + 1) * sizeof(pthread_t));
+    loadCapacity = (int*)        malloc (runways * sizeof(int));
+    busy         = (int*)        malloc (runways * sizeof(int));
 
     for(int i=0; i<runways; i++){
         scanf("%d", &loadCapacity[i]);
@@ -104,7 +162,11 @@ int main(){
                 }
             }
             if(FOR_DEPARTURE == 1){
-                if(use_backup_check){}
+                if(use_backup_check){
+                    pthread_join(threads[runways], NULL);
+                    struct ThreadArgs threadArg = {runways, 1, r};
+                    pthread_create(&threads[runways], NULL, useRunways, (void *)&threadArg);
+                }
                 else{
                     int idx=-1;
                     while(idx == -1){
@@ -115,13 +177,46 @@ int main(){
                             }
                         }
                     }
-                    
+                    for(int i=0; i<runways; i++){
+                        if(!busy[i] ){
+                            if(loadCapacity[i] < loadCapacity[idx]){
+                                if(loadCapacity[i] >= r.total_weight){
+                                    idx = i;
+                                }
+                            }
+                        }
+                    }
+                    struct ThreadArgs threadArg = {idx, 1, r};
+                    pthread_create(&threads[idx], NULL, useRunways, (void *)&threadArg);
                 }
             }
             else if(FOR_DEPARTURE == 0){
-                if(use_backup_check){}
+                if(use_backup_check){
+                    pthread_join(threads[runways], NULL);
+                    struct ThreadArgs threadArg = {runways, 0, r};
+                    pthread_create(&threads[runways], NULL, useRunways, (void *)&threadArg);
+                }
                 else{
-
+                    int idx=-1;
+                    while(idx == -1){
+                        for(int i=0; i<runways; i++){
+                            if(loadCapacity[i]>= r.total_weight || !busy[i]){
+                                idx = i;
+                                break;
+                            }
+                        }
+                    }
+                    for(int i=0; i<runways; i++){
+                        if(!busy[i] ){
+                            if(loadCapacity[i] < loadCapacity[idx]){
+                                if(loadCapacity[i] >= r.total_weight){
+                                    idx = i;
+                                }
+                            }
+                        }
+                    }
+                    struct ThreadArgs threadArg = {idx, 0, r};
+                    pthread_create(&threads[idx], NULL, useRunways, (void *)&threadArg);
                 }
             }
             else{
@@ -137,10 +232,7 @@ int main(){
     }
 
 
-
-
-
-
+    free(threads);
     free(loadCapacity);
     free(busy);
 
